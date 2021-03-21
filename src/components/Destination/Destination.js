e4c6f338c3cab47892f0d5f43c51c68622176575
Tareq Hassan <tareqhassan2014@gmail.com>
2021-03-21 12:10:46 +0600

    destination box style

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router';
 import { useForm } from "react-hook-form";
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Grid, Paper, TextField } from "@material-ui/core";
+import { Box, Button, Grid, Paper, TextField, Typography } from "@material-ui/core";
 import { useStyles } from "@material-ui/pickers/views/Calendar/SlideTransition";
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 
 const paperStyle = { padding: "30px 20px", width: 340 }
 
@@ -21,6 +22,7 @@ const Destination = () => {
             .then(data => setTransport(data))
     }, [])
 
+
     // eslint-disable-next-line eqeqeq
     const vehicle = transport.find(vehicle => vehicle.id == id)
 
@@ -30,19 +32,24 @@ const Destination = () => {
         display: true
     })
 
-    console.log(serchBoxStyle.display);
+
+
+    const [destination, setDestination] = useState({})
+
+
+
+
 
     const { register, handleSubmit, errors } = useForm();
     // console.log(logedInUser);
 
-
     const onSubmit = data => {
-        console.log(data);
-        // console.log(errors);
+
+        setDestination(data);
+        console.log(destination);
         const newBoxStyle = { ...serchBoxStyle }
         newBoxStyle.display = false;
         setSerchBoxStyle(newBoxStyle)
-
     }
 
 
@@ -54,50 +61,57 @@ const Destination = () => {
             <div>
                 <Grid >
                     <Paper style={paperStyle}>
-                        <Box component="span" display={serchBoxStyle.display && 'block'}>
-                            <form onSubmit={handleSubmit(onSubmit)} >
+                        {
+                            serchBoxStyle.display ?
+                                <form onSubmit={handleSubmit(onSubmit)} >
 
-                                <TextField
-                                    style={{ marginBottom: 10 }}
-                                    placeholder="Where you want to start"
-                                    label="Pick From"
-                                    fullWidth
-                                    className={classes.inputField}
-                                    name='from'
-                                    inputRef={register({
-                                        required: 'Please fill up'
-                                    })}
-                                    error={Boolean(errors.from)}
-                                    helperText={errors.from?.message}
-                                />
+                                    <TextField
+                                        style={{ marginBottom: 10 }}
+                                        placeholder="Where you want to start"
+                                        label="Pick From"
+                                        fullWidth
+                                        className={classes.inputField}
+                                        name='from'
+                                        inputRef={register({
+                                            required: 'Please fill up'
+                                        })}
+                                        error={Boolean(errors.from)}
+                                        helperText={errors.from?.message}
+                                    />
 
-                                <TextField
-                                    style={{ marginBottom: 10 }}
-                                    placeholder="Where You want to go"
-                                    label="Pick to"
-                                    fullWidth
-                                    className={classes.inputField}
-                                    name='to'
-                                    inputRef={register({
-                                        required: 'Please fill up'
-                                    })}
-                                    error={Boolean(errors.to)}
-                                    helperText={errors.to?.message}
-                                />
+                                    <TextField
+                                        style={{ marginBottom: 10 }}
+                                        placeholder="Where You want to go"
+                                        label="Pick to"
+                                        fullWidth
+                                        className={classes.inputField}
+                                        name='to'
+                                        inputRef={register({
+                                            required: 'Please fill up'
+                                        })}
+                                        error={Boolean(errors.to)}
+                                        helperText={errors.to?.message}
+                                    />
 
 
-                                <Button
-                                    style={{ marginTop: 20 }}
-                                    variant="contained"
-                                    color="primary"
-                                    type="submit"
-                                    className={classes.inputField}
-                                    fullWidth
-                                >
-                                    Search
-                                </Button>
-                            </form>
-                        </Box>
+                                    <Button
+                                        style={{ marginTop: 20 }}
+                                        variant="contained"
+                                        color="primary"
+                                        type="submit"
+                                        className={classes.inputField}
+                                        fullWidth
+                                    >
+                                        Search
+                                    </Button>
+                                </form> :
+                                <Box>
+                                    <Box boxShadow={3} style={{ margin: '10px 0px' }}><img src={vehicle?.photoURL} alt="" style={{ width: 50 }} /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>{vehicle?.name || "vehicle no't select"}</Typography><SupervisorAccountIcon /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>4</Typography><Typography style={{ display: 'inline-block', marginLeft: '120px' }}>$64</Typography></Box>
+                                    <Box boxShadow={3} style={{ margin: '10px 0px' }}><img src={vehicle?.photoURL} alt="" style={{ width: 50 }} /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>{vehicle?.name || "vehicle no't select"}</Typography><SupervisorAccountIcon /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>4</Typography><Typography style={{ display: 'inline-block', marginLeft: '120px' }}>$64</Typography></Box>
+                                    <Box boxShadow={3} style={{ margin: '10px 0px' }}><img src={vehicle?.photoURL} alt="" style={{ width: 50 }} /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>{vehicle?.name || "vehicle no't select"}</Typography><SupervisorAccountIcon /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>4</Typography><Typography style={{ display: 'inline-block', marginLeft: '120px' }}>$64</Typography></Box>
+                                    <Box boxShadow={3} style={{ margin: '10px 0px' }}><img src={vehicle?.photoURL} alt="" style={{ width: 50 }} /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>{vehicle?.name || "vehicle no't select"}</Typography><SupervisorAccountIcon /><Typography style={{ display: 'inline-block', margin: '0px 5px' }}>4</Typography><Typography style={{ display: 'inline-block', marginLeft: '120px' }}>$64</Typography></Box>
+                                </Box>
+                        }
                     </Paper>
                 </Grid>
             </div>
